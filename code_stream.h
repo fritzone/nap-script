af7cc1b9994e7cd7d4a596a18ad2046bb01f6ad6
fritzone <736764+fritzone@users.noreply.github.com>
2012-08-13 14:49:07 +0200

    more changes for the unified code output

---
+++ code_stream.h
@@ -0,0 +1,32 @@
+#ifndef CODE_STREAM_H
+#define CODE_STREAM_H
+
+static const char SPACE = ' ';
+static const char NEWLINE = '\n';
+
+class code_stream
+{
+public:
+
+    code_stream& operator << (const char* s)
+    {
+        printf("%s", s);
+        return *this;
+    }
+
+    code_stream& operator << (char c)
+    {
+        printf("%c", c);
+        return *this;
+    }
+
+    code_stream& operator << (int i)
+    {
+        printf("%i", i);
+        return *this;
+    }
+};
+
+
+
+#endif // CODE_STREAM_H
