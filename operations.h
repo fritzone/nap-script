00df98a487e1bf60cbe201a7f5d3c50859b061e2
fritzone <fritzone@gmail.com>
2013-10-05 22:12:14 +0200

    removed files

--- operations.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OPERATIONS_H_
-#define _OPERATIONS_H_
-
-#include "envelope.h"
-#include "method.h"
-
-void do_list_assignment( envelope* rvalue, variable* var, int level, const method* the_method, call_context* cc, int reqd_type);
-
-#endif
