2eea7f18d7b0953715b7c967476205ea145b9bd9
fritzone <fritzone@gmail.com>
2013-11-05 14:40:16 +0100

    more unit tests

--- tests/test.c
+++ tests/test.c
@@ -1,8 +1,32 @@
 #include <check.h>
+#include <stdlib.h>
 
-START_TEST (test_name)
+START_TEST(test_vm_push_immediate)
 {
-      /* unit test code */
+    ck_assert_int_eq(0, 0);
 }
 END_TEST
 
+Suite* suite_vm_push()
+{
+    Suite* s = suite_create("push");
+
+    /* Core test case */
+    TCase *tc_immediate = tcase_create ("immediate");
+    tcase_add_test (tc_immediate, test_vm_push_immediate);
+    suite_add_tcase (s, tc_immediate);
+
+    return s;
+}
+
+
+int main ()
+{
+    int number_failed = 0;
+    Suite *s = suite_vm_push();
+    SRunner *sr = srunner_create (s);
+    srunner_run_all (sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed (sr);
+    srunner_free (sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
