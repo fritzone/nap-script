4caabbaef72b791dacf36d2cee784364ef461bfb
fritzone <fritzone@gmail.com>
2013-11-15 14:55:41 +0100

    small test change

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,21 +4,24 @@
 #include "nap_runtime.h"
 #include <stdlib.h>
 
-TEST_CASE("Variables", "[vm_variables]")
-{
-	nap_runtime* runtime = nap_runtime_create(0);
-    REQUIRE(runtime != 0);
+    #define SCRIPT_START nap_runtime* runtime = nap_runtime_create(0);              \
+                     nap_bytecode_chunk* bytecode = nap_runtime_compile(runtime,\
+                     
 
-    nap_bytecode_chunk* bytecode = nap_runtime_compile(runtime, 
-                     "                            \
-                      int a;                      \
-                      a = 2;                      \
-                     "
-                    );
-    REQUIRE(bytecode != 0);
-    int t = nap_runtime_execute(runtime, bytecode);
-    REQUIRE(1 == t);
+    #define SCRIPT_END  ); \
+                    int t = nap_runtime_execute(runtime, bytecode); \
+                    REQUIRE(1 == t); 
+
+    TEST_CASE("Variables", "[vm_variables]")
+    {
+    SCRIPT_START                    
+    "                               \
+        int a;                      \
+        a = 2;                      \
+    "                               
+    SCRIPT_END
+    
     REQUIRE(2 == nap_runtime_get_int(runtime, "a"));
     free(runtime);
-}
+    }
 
