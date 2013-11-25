c7030c2e01472db1ce06c4de062c95a365b117ff
fritzone <736764+fritzone@users.noreply.github.com>
2013-11-25 20:54:49 +0100

    switched to gtest

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,28 +1,26 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.h"
-
 #include "nap_runtime.h"
 #include "utils.h"
+#include "gtest/gtest.h"
 
 #define SCRIPT_START \
     nap_runtime* runtime = nap_runtime_create(0);                  \
-    REQUIRE(runtime != NULL);                                      \
+    EXPECT_FALSE(runtime == NULL);                                      \
     nap_bytecode_chunk* bytecode = nap_runtime_compile(runtime,    
     
 #define SCRIPT_END \
     );                                                             \
-    REQUIRE(bytecode != NULL);                                     \
+    EXPECT_FALSE(bytecode == NULL);                                     \
     int t = nap_runtime_execute(runtime, bytecode);                \
-    REQUIRE(1 == t);                                               
+    ASSERT_EQ(1, t);                                               
 
 
 #define VAR_INT(a)    nap_runtime_get_int(runtime, #a )
 
 #define SCRIPT_SHUTDOWN \
     nap_runtime_shutdown(&runtime);                                \
-    REQUIRE(runtime == 0);
+    EXPECT_TRUE(runtime == NULL);
     
-TEST_CASE("Variable definitions", "[variables]")
+TEST(VariableDefinitions, SimpleInt)
 {
     SCRIPT_START
     "                               \
@@ -31,12 +29,13 @@ TEST_CASE("Variable definitions", "[variables]")
     "                               
     SCRIPT_END
     
-    REQUIRE(2 == VAR_INT(a));
+    ASSERT_EQ(2, VAR_INT(a));
 
     SCRIPT_SHUTDOWN;
 }
 
-TEST_CASE("Operations on variables", "[operations][variables]")
+
+TEST(Operations, BasicVariableOperations)
 {
 
     SCRIPT_START
@@ -58,18 +57,18 @@ TEST_CASE("Operations on variables", "[operations][variables]")
     "                               
     SCRIPT_END
 
-    REQUIRE( 9 == VAR_INT(a));
-    REQUIRE( 3 == VAR_INT(b));
-    REQUIRE(12 == VAR_INT(a_plus_b));
-    REQUIRE( 6 == VAR_INT(a_minus_b));
-    REQUIRE( 3 == VAR_INT(a_div_b));
-    REQUIRE(27 == VAR_INT(a_mul_b));
-    REQUIRE( 0 == VAR_INT(a_mod_b));
+    ASSERT_TRUE( 9 == VAR_INT(a));
+    ASSERT_TRUE( 3 == VAR_INT(b));
+    ASSERT_TRUE(12 == VAR_INT(a_plus_b));
+    ASSERT_TRUE( 6 == VAR_INT(a_minus_b));
+    ASSERT_TRUE( 3 == VAR_INT(a_div_b));
+    ASSERT_TRUE(27 == VAR_INT(a_mul_b));
+    ASSERT_TRUE( 0 == VAR_INT(a_mod_b));
 
     SCRIPT_SHUTDOWN
 }
 
-TEST_CASE("Operations on immediate values", "[operations][immediates]")
+TEST(Operations, BasicImmediateOperations)
 {
     SCRIPT_START
     "                               \
@@ -86,31 +85,31 @@ TEST_CASE("Operations on immediate values", "[operations][immediates]")
     "                               
     SCRIPT_END
     
-    REQUIRE(12 == VAR_INT(a_plus_b));
-    REQUIRE( 6 == VAR_INT(a_minus_b));
-    REQUIRE( 3 == VAR_INT(a_div_b));
-    REQUIRE(27 == VAR_INT(a_mul_b));
-    REQUIRE( 0 == VAR_INT(a_mod_b));
+    ASSERT_TRUE(12 == VAR_INT(a_plus_b));
+    ASSERT_TRUE( 6 == VAR_INT(a_minus_b));
+    ASSERT_TRUE( 3 == VAR_INT(a_div_b));
+    ASSERT_TRUE(27 == VAR_INT(a_mul_b));
+    ASSERT_TRUE( 0 == VAR_INT(a_mod_b));
     
     SCRIPT_SHUTDOWN
 }
 
-TEST_CASE("Testing of invalid variable name", "[error][compile]")
+TEST(Definitions, InvalidVariableName)
 {
     nap_runtime* runtime = nap_runtime_create(0);
-    REQUIRE(runtime != NULL);
+    ASSERT_TRUE(runtime != NULL);
     nap_bytecode_chunk* bytecode = nap_runtime_compile(runtime,
     "              \
     int a_ plus_b; \
     "
     );
 
-    REQUIRE(bytecode == NULL);
+    ASSERT_TRUE(bytecode == NULL);
     SCRIPT_SHUTDOWN
 }
 
 
-TEST_CASE("Post, pre increment", "[operations][post][pre][increment")
+TEST(Operations, PostPreIncrement)
 {
     SCRIPT_START
             "               \
@@ -120,14 +119,14 @@ TEST_CASE("Post, pre increment", "[operations][post][pre][increment")
             "
     SCRIPT_END
 
-    REQUIRE(2 == VAR_INT(i));
-    REQUIRE(0 == VAR_INT(y));
-    REQUIRE(2 == VAR_INT(z));
+    ASSERT_TRUE(2 == VAR_INT(i));
+    ASSERT_TRUE(0 == VAR_INT(y));
+    ASSERT_TRUE(2 == VAR_INT(z));
 
     SCRIPT_SHUTDOWN
 }
 
-TEST_CASE("Function definition", "[function]")
+TEST(Functions, MessyCodeTest)
 {
     SCRIPT_START
     "                             \
@@ -153,7 +152,8 @@ TEST_CASE("Function definition", "[function]")
     int y = 1;                    \
     "
     SCRIPT_END
-    REQUIRE( 5 == VAR_INT(g));
+    ASSERT_TRUE( 5 == VAR_INT(g));
 
     SCRIPT_SHUTDOWN
 }
+
