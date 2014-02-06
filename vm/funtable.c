9df3458f9bf3065d593dde9eb9ac87abef0319d6
fritzone <fritzone@localhost>
2014-02-06 20:51:33 +0100

    Fixed for windows

--- vm/funtable.c
+++ vm/funtable.c
@@ -1,6 +1,7 @@
 #include "funtable.h"
 #include "nap_consts.h"
-int interpret_funtable(nap_vm *vm, uint8_t *start_location, uint32_t len)
+
+int interpret_funtable(struct nap_vm *vm, uint8_t *start_location, uint32_t len)
 {
     return NAP_SUCCESS;
 }
