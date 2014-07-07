cdedf070e016944ff3b15bdf33581f3b5e91d60f
fritzone <fritzone@localhost>
2014-07-07 15:23:16 +0200

    some fixes for some unary operators

--- vm/dec.c
+++ vm/dec.c
@@ -10,8 +10,8 @@
 
 int nap_dec(struct nap_vm* vm)
 {
-    uint8_t inc_what = vm->content[nap_step_ip(vm)]; /* variable, register*/
-    if(inc_what == OPCODE_VAR)
+    uint8_t dec_what = vm->content[nap_step_ip(vm)]; /* variable, register*/
+    if(dec_what == OPCODE_VAR)
     {
         nap_index_t var_index = nap_fetch_index(vm);
         struct variable_entry* ve = nap_fetch_variable(vm, var_index);
@@ -22,6 +22,108 @@ int nap_dec(struct nap_vm* vm)
             (*(nap_int_t*)ve->instantiation->value) --;
         }
         else
+        if(ve->instantiation->type == OPCODE_BYTE)
+        {
+            (*(nap_byte_t*)ve->instantiation->value) --;
+        }
+        else
+        if(ve->instantiation->type == OPCODE_REAL)
+        {
+            (*(nap_real_t*)ve->instantiation->value) --;
+        }
+        else
+        {
+            NAP_NOT_IMPLEMENTED
+        }
+    }
+    else
+    if(dec_what == OPCODE_REG)
+    {
+        uint8_t register_type = vm->content[nap_step_ip(vm)]; /* int/string/float...*/
+
+        /* we are dealing with an INT type register */
+        if(register_type == OPCODE_INT)  /* to handle mov reg int x, <something> */
+        {
+            uint8_t register_index = vm->content[nap_step_ip(vm)]; /* 0, 1, 2 ...*/
+            vm->cec->regi[register_index] --;
+        }
+        else
+        if(register_type == OPCODE_IDX)
+        {
+            uint8_t register_index = vm->content[nap_step_ip(vm)]; /* 0, 1, 2 ...*/
+            vm->cec->regidx[register_index] --;
+        }
+        else
+        if(register_type == OPCODE_BYTE)
+        {
+            uint8_t register_index = vm->content[nap_step_ip(vm)]; /* 0, 1, 2 ...*/
+            vm->cec->regb[register_index] --;
+        }
+        else
+        if(register_type == OPCODE_REAL)
+        {
+            uint8_t register_index = vm->content[nap_step_ip(vm)]; /* 0, 1, 2 ...*/
+            vm->cec->regr[register_index] --;
+        }
+        else
+        {
+            NAP_NOT_IMPLEMENTED
+        }
+    }
+    else
+    if(dec_what == OPCODE_CCIDX) /* decreasing an indexed*/
+    {
+        uint8_t ccidx_target = vm->content[nap_step_ip(vm)];  /* should be a variable */
+        if(ccidx_target == OPCODE_VAR)
+        {
+            nap_index_t var_index = nap_fetch_index(vm);
+            struct variable_entry* var = nap_fetch_variable(vm, var_index);
+            uint8_t ctr_used_index_regs = 0;
+            char* error = NULL;
+            int64_t idx = -1;
+
+            ASSERT_NOT_NULL_VAR(var)
+            CHECK_VARIABLE_INSTANTIATON(var)
+
+            /* now should come the index reg counter of vm->ccidx,
+               a simple byte since there are max 256 indexes */
+            ctr_used_index_regs = vm->content[nap_step_ip(vm)];
+            idx = deliver_flat_index(vm, var, ctr_used_index_regs, &error);
+
+            if(idx < 0) /* error? */
+            {
+                /* do not use the set_error here, the string was allocated
+                   copying it would be a memory leak*/
+                vm->error_description = error;
+                return NAP_FAILURE;
+            }
+
+            /* decreasing an indexed integer */
+            if(var->instantiation->type == OPCODE_INT)
+            {
+                /* casting it to nap_int_t is ok, since
+                 * var->instantiation->type == OPCODE_INT so we have
+                 * allocated nap_int_t variable in the grow */
+                ((nap_int_t*)var->instantiation->value)[idx] --;
+            }
+            else
+            /* decreasing an indexed byte */
+            if(var->instantiation->type == OPCODE_BYTE)
+            {
+                ((nap_byte_t*)var->instantiation->value)[idx] --;
+            }
+            else
+            /* decreasing an indexed real */
+            if(var->instantiation->type == OPCODE_REAL)
+            {
+                ((nap_real_t*)var->instantiation->value)[idx] --;
+            }
+            else
+            {
+                NAP_NOT_IMPLEMENTED /* No other type of indexables */
+            }
+        }
+        else /* what else can be indexed? */
         {
             NAP_NOT_IMPLEMENTED
         }
--- vm/unary.c
+++ vm/unary.c
@@ -31,11 +31,48 @@ int nap_unary(struct nap_vm* vm)
                 (*(nap_int_t*)var->instantiation->value) = !(*(nap_int_t*)var->instantiation->value);
             }
             else
+            if(vm->cec->current_opcode == OPCODE_NEG)
+            {
+                (*(nap_int_t*)var->instantiation->value) = (*(nap_int_t*)var->instantiation->value);
+            }
+            else
+            if(vm->cec->current_opcode == OPCODE_POS)
+            {
+                (*(nap_int_t*)var->instantiation->value) = llabs((*(nap_int_t*)var->instantiation->value));
+            }
+            else
+            {
+                NAP_NOT_IMPLEMENTED  /* No other type of unary operation */
+            }
+        }
+        else
+        if(var->instantiation->type == OPCODE_BYTE)
+        {
+            if(vm->cec->current_opcode == OPCODE_BCOM)
+            {
+                (*(nap_byte_t*)var->instantiation->value) = ~(*(nap_byte_t*)var->instantiation->value);
+            }
+            else
+            if(vm->cec->current_opcode == OPCODE_NOT)
+            {
+                (*(nap_byte_t*)var->instantiation->value) = !(*(nap_byte_t*)var->instantiation->value);
+            }
+            else
+            if(vm->cec->current_opcode == OPCODE_NEG)
+            {
+                (*(nap_byte_t*)var->instantiation->value) = (*(nap_byte_t*)var->instantiation->value);
+            }
+            else
+            if(vm->cec->current_opcode == OPCODE_POS) /* TODO: does this make sense? A byte is always positive. */
+            {
+                (*(nap_byte_t*)var->instantiation->value) = abs((*(nap_byte_t*)var->instantiation->value));
+            }
+            else /* No other type of unary operation */
             {
                 NAP_NOT_IMPLEMENTED
             }
         }
-        else
+        else /* Unary operations on other data types are not allowed */
         {
             NAP_NOT_IMPLEMENTED
         }
@@ -57,11 +94,49 @@ int nap_unary(struct nap_vm* vm)
                 nap_set_regi(vm, register_index, ! nap_regi(vm, register_index));
             }
             else
+            if(vm->cec->current_opcode == OPCODE_NEG)
+            {
+                nap_set_regi(vm, register_index, - nap_regi(vm, register_index));
+            }
+            else
+            if(vm->cec->current_opcode == OPCODE_POS)
+            {
+                nap_set_regi(vm, register_index, llabs(nap_regi(vm, register_index)));
+            }
+            else
             {
                 NAP_NOT_IMPLEMENTED
             }
         }
         else
+        if(register_type == OPCODE_BYTE) /* unary operation on a byte register */
+        {
+            uint8_t register_index = vm->content[nap_step_ip(vm)]; /* 0, 1, 2 ...*/
+            if(vm->cec->current_opcode == OPCODE_BCOM)
+            {
+                nap_set_regb(vm, register_index, ~ nap_regb(vm, register_index));
+            }
+            else
+            if(vm->cec->current_opcode == OPCODE_NOT)
+            {
+                nap_set_regb(vm, register_index, ! nap_regb(vm, register_index));
+            }
+            else
+            if(vm->cec->current_opcode == OPCODE_NEG)
+            {
+                nap_set_regb(vm, register_index, - nap_regb(vm, register_index));
+            }
+            else
+            if(vm->cec->current_opcode == OPCODE_POS) /* TODO: does this make sense? */
+            {
+                nap_set_regb(vm, register_index, abs(nap_regb(vm, register_index)));
+            }
+            else
+            {
+                NAP_NOT_IMPLEMENTED
+            }
+        }
+        else /* Unary operations on other types are not allowed */
         {
             NAP_NOT_IMPLEMENTED
         }
