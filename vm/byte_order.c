f29c7b8f9921389a27893843f4dd8851e4f9229b
fritzone <736764+fritzone@users.noreply.github.com>
2014-10-20 15:14:39 +0200

    reverting to little endian encoding

--- vm/byte_order.c
+++ vm/byte_order.c
@@ -1,10 +1,14 @@
 #include "byte_order.h"
 
+/* all this gets converted to little endian */
+
+#ifndef COMPILED_ON_LITTLE_ENDIAN
+
 uint32_t htovm_32(uint32_t l)
 {
     U4 mask,res;
     unsigned char* p = (unsigned char*)&l;
-    mask.l = 0x00010203;
+    mask.l = 0x03020100;
 #ifdef _WINDOWS
 #pragma warning(suppress: 6386)
 #endif
@@ -22,8 +26,8 @@ uint64_t htovm_64(uint64_t ll)
     U8 res;
 
     unsigned char* p = (unsigned char*)&ll;
-    mask1.s = 0x0001;
-    mask2.l = 0x00010203;
+    mask1.s = 0x0100;
+    mask2.l = 0x03020100;
 #ifdef _WINDOWS
 #pragma warning(suppress: 6386)
 #endif
@@ -45,8 +49,10 @@ uint16_t htovm_16(uint16_t s)
 {
     U2 mask,res;
     unsigned char* p = (unsigned char*)&s;
-    mask.s = 0x0001;
+    mask.s = 0x0100;
     res.c[mask.c[0]] = p[0];
     res.c[mask.c[1]] = p[1];
     return res.s;
 }
+
+#endif
--- vm/byte_order.h
+++ vm/byte_order.h
@@ -3,29 +3,39 @@
 
 #include <stdint.h>
 
-typedef union{
-    uint8_t c[4];
-    uint16_t s[2];
-    uint32_t l;
-}U4;
+#ifdef COMPILED_ON_LITTLE_ENDIAN
 
-typedef union{
-    uint8_t c[8];
-    uint8_t c2[2][4];
-    uint16_t s[4];
-    uint32_t l[2];
-    uint64_t ll;
-}U8;
+#define htovm_16(s) s
+#define htovm_32(l) l
+#define htovm_64(ll) ll
 
-typedef union{
-    uint8_t c[2];
-    uint16_t s;
-}U2;
+#else
+    typedef union{
+        uint8_t c[4];
+        uint16_t s[2];
+        uint32_t l;
+    }U4;
 
-uint16_t htovm_16(uint16_t s);
+    typedef union{
+        uint8_t c[8];
+        uint8_t c2[2][4];
+        uint16_t s[4];
+        uint32_t l[2];
+        uint64_t ll;
+    }U8;
 
-uint32_t htovm_32(uint32_t l);
+    typedef union{
+        uint8_t c[2];
+        uint16_t s;
+    }U2;
 
-uint64_t htovm_64(uint64_t ll);
+    uint16_t htovm_16(uint16_t s);
+
+    uint32_t htovm_32(uint32_t l);
+
+    uint64_t htovm_64(uint64_t ll);
+#endif
+
+
 
 #endif
